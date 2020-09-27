92adf6d5e10d012f0975a93ff2db5bb9a7cfe6fd
Francesco Bellini <francescobellini92@gmail.com>
2020-09-27 15:41:03 +0200

    Remove separated controllers

---
+++ src/server/controllers/controllers.js
@@ -0,0 +1,62 @@
+const { 
+  API_KEY_GEO, 
+  API_GEO_BASEURL,  
+  API_KEY_WEATHERBIT, 
+  API_WEATHERBIT_BASEURL, 
+  API_KEY_PIXABAY, 
+  API_PIXABAY_BASEURL 
+} = require('../environment');
+
+const { 
+  makeGeoResponse, 
+  makePixResponse, 
+  makeWeatherResponse, 
+  EmptyResponseError 
+} = require('../factories/response-factory');
+
+const fetch = require('node-fetch');
+
+async function getDataFromAPI(req, urlBuilderFn, factoryFn, emptyResponseFallback = []) {
+  try {
+    const url = urlBuilderFn(req.query);
+    const request = await fetch(url);
+    const response = await request.json();
+    const parsedResponse = factoryFn(response);
+    return parsedResponse;
+  } catch (e) {
+    if (e instanceof EmptyResponseError) {
+      return emptyResponseFallback;
+    }
+    throw e;
+  }
+};
+
+async function getGeoData(req) {
+  return getDataFromAPI(
+    req,
+    (queryParams) => `${API_GEO_BASEURL}?username=${API_KEY_GEO}&name=${queryParams.location}`,
+    makeGeoResponse
+  );
+}
+
+async function getWeatherData(req) {
+  return getDataFromAPI(
+    req,
+    (queryParams) => `${API_WEATHERBIT_BASEURL}?lat=${queryParams.lat}&lon=${queryParams.lon}&days=${queryParams.days}&key=${API_KEY_WEATHERBIT}`,
+    makeWeatherResponse
+  );
+}
+
+async function getPixData(req) {
+  return getDataFromAPI(
+    req,
+    (queryParams) => `${API_PIXABAY_BASEURL}?key=${API_KEY_PIXABAY}&q=${queryParams.location}&category=places&editors_choice=true&per_page=10`,
+    makePixResponse
+  );
+}
+
+module.exports = {
+  getGeoData,
+  getWeatherData,
+  getPixData
+};
