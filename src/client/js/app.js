456d8b5d314623786eb965463194b1f627b15f92
Francesco Bellini <francescobellini92@gmail.com>
2020-10-03 18:58:08 +0200

    Start adding router features

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -16,38 +16,38 @@ export default () => {
   const tripForm = $('#trip-form');
   const tripList = $('#trip-list');
   const tripCardContainer = $('#card-container');
+  const home = $('#home');
   const analysisContainer = $('#analysis-container');
   const confidenceInput = $('#confidence');
   const scoreInput = $('#score');
   const agreementInput = $('#agreement');
   const subjectivityInput = $('#subjectivity');
   const ironyInput = $('#irony');
-  tripList.updateMany(tripService.trips);
+  tripList.updateProps(tripService.trips);
   initCards();
 
   function initCards() {
     const fragment = document.createDocumentFragment();
     tripService.trips.forEach(trip => {
       const tripCard = new TripCardComponent();
-      tripCard.update(trip.general)
+      tripCard.updateProps(trip)
       fragment.appendChild(tripCard);
     })
     tripCardContainer.appendChild(fragment);
   }
 
-
   const addTripReq$ = new Observable(async([name, start, end, location]) => {
       show(loader);
       const newTrip = await tripService.add(name, start, end, location);
-      debugger;
       return newTrip;
   });
-
-  addTripReq$.subscribe((data) => {
-    debugger;
+  addTripReq$.subscribe((trip) => {
     hide(loader);
     tripForm.reset();
-    tripList.update(data.general);
+    const tripCard = new TripCardComponent();
+    tripCard.updateProps(trip)
+    tripCardContainer.appendChild(tripCard);
+    tripList.add(trip.general);
   });
 
   async function onSubmit({detail}) {
@@ -83,10 +83,31 @@ export default () => {
     // }
   }
 
-  async function onRemove({detail}) {
+  function onRemove({detail}) {
     const tripId = detail;
     tripService.delete(tripId);
   }
+
+  function onNavigation() {
+    const hash = window.location.hash ?? '#home';
+    routeManager[hash]();
+  }
+
+  const routeManager = {
+    '#home': () => {
+      show(home);
+    }
+  }
+
+  function initApp() {
+    onNavigation();
+  }
+
   tripForm.addEventListener('submit', onSubmit);
   tripList.addEventListener('remove', onRemove)
+  tripCardContainer.addEventListener('remove', onRemove)
+  window.addEventListener('popstate', onNavigation);
+  window.addEventListener('hashchange', onNavigation);
+
+  initApp();
 }
