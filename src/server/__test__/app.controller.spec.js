7773906546260c284c20b0e444b6ef2e0cddbf75
Francesco Bellini <francescobellini92@gmail.com>
2020-10-06 20:33:59 +0200

    Add  backend end-to-end tests

---
+++ src/server/__test__/app.controller.spec.js
@@ -0,0 +1,68 @@
+const app = require('../controller/app.controller');
+const supertest = require('supertest');
+const moment = require('moment');
+const request = supertest(app);
+
+describe('Server API', () => {
+    
+    const today = moment().toISOString();
+    const tomorrow = moment().add(1, 'days').toISOString();
+    const realLocation = 'London';
+    const fakeLocation = 'Foo bar';
+    const name = 'My trip';
+
+    const badDatesPayload = {error: 'bad dates'};
+    const badLocationPayload = {error: 'bad location'};
+    const badQueryParamsPayload = {error: 'missing required query parameters'};
+
+    const getQueryParams = (start, end, location, name) => `?start=${start}&end=${end}&location=${location}&name=${name}`;
+        
+    it('responds 200 to good request', (done) => {
+        const queryParams = getQueryParams(today, tomorrow, realLocation, name);
+        request.get(`/trip-info${queryParams}`)
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+
+    it('responds 400 to request with bad dates', (done) => {
+        const queryParams = getQueryParams(tomorrow, today, realLocation, name);
+        request.get(`/trip-info${queryParams}`)
+        .expect('Content-Type', /json/)
+        .expect(400, badDatesPayload, done);
+    });
+
+    it('responds 400 to request with bad locations', (done) => {
+        const queryParams = getQueryParams(today, tomorrow, fakeLocation, name);
+        request.get(`/trip-info${queryParams}`)
+        .expect('Content-Type', /json/)
+        .expect(400, badLocationPayload, done);
+    });
+
+    it('responds 400 to request with missing parameter start', (done) => {
+        const queryParams = getQueryParams('', tomorrow, realLocation, name);
+        request.get(`/trip-info${queryParams}`)
+        .expect('Content-Type', /json/)
+        .expect(400, badQueryParamsPayload, done);
+    });
+
+    it('responds 400 to request with missing parameter end', (done) => {
+        const queryParams = getQueryParams(today, '', realLocation, name);
+        request.get(`/trip-info${queryParams}`)
+        .expect('Content-Type', /json/)
+        .expect(400, badQueryParamsPayload, done);
+    });
+
+    it('responds 400 to request with missing parameter location', (done) => {
+        const queryParams = getQueryParams(today, tomorrow, '', name);
+        request.get(`/trip-info${queryParams}`)
+        .expect('Content-Type', /json/)
+        .expect(400, badQueryParamsPayload, done);
+    });
+
+    it('responds 400 to request with missing parameter name', (done) => {
+        const queryParams = getQueryParams(today, tomorrow, realLocation, '');
+        request.get(`/trip-info${queryParams}`)
+        .expect('Content-Type', /json/)
+        .expect(400, badQueryParamsPayload, done);
+    });
+});
