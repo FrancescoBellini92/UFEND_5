7b3225077450c3d1d5978774cb8e064dcea88016
Francesco Bellini <francescobellini92@gmail.com>
2020-11-02 20:01:37 +0100

    Update components registration and index

--- src/client/js/components/components.module.ts
+++ src/client/js/components/components.module.ts
@@ -2,6 +2,7 @@ import WebComponent from "../base/web.component";
 import AddPageComponent from "./add-page/add-page.component";
 import DetailPageComponent from "./detail-page/detail-page.component";
 import HomePageComponent from "./home-page/home-page.component";
+import ToastComponent from "./toast/toast.component";
 import TripCardComponent from "./trip-card/trip-card.component";
 import TripFormComponent from "./trip-form/trip-form.component";
 import TripListComponent from "./trip-list/trip-list.component";
@@ -10,10 +11,11 @@ function registerComponents(...components: typeof WebComponent[]) {
   components.forEach(component => component.define(component));
 }
 export default () => registerComponents(
+  ToastComponent,
   TripCardComponent,
+  TripListComponent,
   HomePageComponent,
   DetailPageComponent,
   TripFormComponent,
-  AddPageComponent,
-  TripListComponent
+  AddPageComponent
 );
