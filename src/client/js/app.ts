aa4c3580251322b4f53ee945371fd99b544db789
Francesco Bellini <francescobellini92@gmail.com>
2020-11-27 23:55:51 +0100

    Add header highlight function to routing

--- src/client/js/app.ts
+++ src/client/js/app.ts
@@ -1,7 +1,13 @@
 import registerComponents from './components/components.module';
 import { navigateTo } from './base/router';
+import router from './base/router';
+import factory from './base/factory';
+import HeaderService from './services/header.service';
+
+const headerService = factory.make<HeaderService>(HeaderService.injectionToken)
 
 export default () => {
   registerComponents();
+  router.addOptionalFn((hash: string) => headerService.highlightNavigation(hash));
   navigateTo();
 }
