21998df5b273103c359723f346a24f6ae3c047fd
Francesco Bellini <francescobellini92@gmail.com>
2021-01-19 00:18:06 +0100

    Improve performance by caching queried elements

--- src/client/js/base/child.ts
+++ src/client/js/base/child.ts
@@ -4,8 +4,11 @@ export const Child: ChildDecorator = (selector, multiple?) => (target, name) => 
   let element: HTMLElement | NodeListOf<Element>;
 
   const getElementFn = function() {
-    console.log(this)
-    return this.hasShadowDom ? this.queryShadowRoot(selector, multiple) : this[multiple ? 'queryselectorAll' : 'querySelector'](selector);
+    const privateElementProperty = `__${name}`
+    if (!this[privateElementProperty]) {
+      this[privateElementProperty] = this.hasShadowDom ? this.queryShadowRoot(selector, multiple) : this[multiple ? 'queryselectorAll' : 'querySelector'](selector);
+    }
+    return this[privateElementProperty];
   }
 
   const setElementFn = (el: HTMLElement) => element = el
